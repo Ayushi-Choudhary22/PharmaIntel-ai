a81bdf1d549ef6c1475f2825c343c22ca44defa5
Ayushi Choudhary <140884639+Ayushi-Choudhary22@users.noreply.github.com>
2025-10-16 19:17:53 +0530

    Add files via upload

---
+++ App.tsx
@@ -0,0 +1,19 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import LandingPage from './pages/LandingPage';
+import AuthPage from './pages/AuthPage';
+import { ThemeProvider } from './context/ThemeContext';
+
+function App() {
+  return (
+    <ThemeProvider>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/auth" element={<AuthPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
+  );
+}
+
+export default App;
